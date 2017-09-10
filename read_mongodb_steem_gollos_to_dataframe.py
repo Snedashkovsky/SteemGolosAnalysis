1086f39f49e424dbfa3c8ed2e3818fc97861a381
Serge Nedashkovsky <snedashkovsky@gmail.com>
2017-09-10 19:04:06 +1000

    Read from MongoDB (Steem|Golos) to pandas DataFrame.
    Example for topics.

---
+++ read_mongodb_steem_gollos_to_dataframe.py
@@ -0,0 +1,17 @@
+from pymongo import MongoClient
+import pandas as pd
+
+mongo = MongoClient()
+db = mongo.steemdb
+
+cursor = db.comment.find({
+    "depth" : 0.0
+}, {
+    "body" : 1.0,
+    "title" : 1.0
+})
+
+comment_df = pd.DataFrame(columns=['title', '_id', 'body'])
+for comment in cursor:
+    comment_df = comment_df.append(pd.DataFrame(comment, index=[len(comment_df)]))
+print('Load topics: ', len(df))
